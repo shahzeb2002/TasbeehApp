111f2a1758d0e24fd70b2c0a502113242d906cf0
Shah Zeb <138509711+shahzeb2002@users.noreply.github.com>
2025-11-21 21:43:00 +0500

    sign in and sing up basic

---
+++ lib/pages/tasbeeh_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class TasbeehPage extends StatefulWidget {
+  const TasbeehPage({super.key});
+
+  @override
+  State<TasbeehPage> createState() => _TasbeehPageState();
+}
+
+class _TasbeehPageState extends State<TasbeehPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
